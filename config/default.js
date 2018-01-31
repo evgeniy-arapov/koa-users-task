e915235b0d2877fc655f5ebf4ace7665f95e5598
Evgeniy Arapov <arapov.evgeniy@gmail.com>
2018-01-31 20:37:42 +0200

    change listen port

--- config/default.js
+++ config/default.js
@@ -2,8 +2,8 @@ module.exports = {
   // secret data can be moved to env variables
   // or a separate config
   secret: "mysecret",
-  port: 3000,
-  testPort: 3001,
+  port: 3002,
+  testPort: 3003,
   dataBaseUri: "mongodb://localhost/koaUserTask",
   testDataBaseUri: "mongodb://localhost/test",
   root: process.cwd()
